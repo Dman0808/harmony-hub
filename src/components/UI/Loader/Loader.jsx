09519adb79287460f5780a6721938af9ee45c191
Alexandrbig1 <alexsmagin1@gmail.com>
2024-10-24 10:41:53 -0500

    fix: update loading styles

--- src/components/UI/Loader/Loader.jsx
+++ src/components/UI/Loader/Loader.jsx
@@ -1,25 +1,19 @@
 import { LoaderDivWrapper } from "./Loader.styled";
-import { RotatingLines, Hourglass } from "react-loader-spinner";
+import { Audio } from "react-loader-spinner";
 
 export default function Loader() {
   return (
     <LoaderDivWrapper>
-      <Hourglass
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="hourglass-loading"
+      <Audio
+        height="100"
+        width="100"
+        ariaLabel="audio-loading"
         wrapperStyle={{}}
-        wrapperClass=""
-        colors={["#306cce", "#72a1ed"]}
-      />
-      {/* <RotatingLines
-        strokeColor="#a2d2ff"
-        strokeWidth="5"
-        animationDuration="0.75"
-        width="96"
+        wrapperClass="wrapper-class"
         visible={true}
-      /> */}
+        color="#f72585"
+      />
+      ;
     </LoaderDivWrapper>
   );
 }
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -7,15 +7,11 @@ export const theme = {
     activeLinkColor: "#000",
     whiteColor: "#fff",
     buttonBgColor: "#4895ef",
-    // buttonBgColor: "#4cc9f0",
     buttonHoverBgColor: "#4cc9f0",
-    // buttonHoverBgColor: "#9b45d6",
     pinkColor: "#f72585",
     blueColor: "#4cc9f0",
     violetColor: "#7209b7",
     redColor: "#ff073a",
     greenColor: "#49ff18",
-    headingGradient:
-      "linear-gradient(90deg, #f72585, #7209b7, #ff073a, #49ff18)",
   },
 };

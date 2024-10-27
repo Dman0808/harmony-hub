5d9f2175a0926ce94f91f4432eafb62815499a62
XhrCkYsSgZokJL <morganculbertson@gmail.com>
2024-10-27 10:49:07 -0500

    fix

--- Styrobob-request.js
+++ Styrobob-request.js
@@ -1,16 +1,23 @@
 // log
 console.log(`${args[0]}/api/generate`);
+console.log(`${args[1]}`);
+
+// build
+const requestData = {
+  "prompt": `${args[1]}`,
+  "make_instrumental": false,
+  "wait_audio": false
+}
+
+// log
+console.log(requestData)
 
 // request
 const sunoGen = Functions.makeHttpRequest({
   url: `${args[0]}/api/generate`,
   method: 'POST',
   headers: { "Content-Type": 'application/json' },
-  data: JSON.stringify({
-    "prompt": args[1],
-    "make_instrumental": false,
-    "wait_audio": true
-  }),
+  data: requestData,
   timeout: 20000
 })
 
